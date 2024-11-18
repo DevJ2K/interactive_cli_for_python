884450180e974e4e1cd51a7270b33e72f0a85e05
DevJ2K <ajavontheopro@gmail.com>
2024-11-18 09:42:04 +0100

    ✅ : Add new tests for remove option.

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -28,3 +28,24 @@ def test_add_invalid_option():
 		cli.add_option(None, "dest", True)
 	with pytest.raises(CLIError):
 		cli.add_option("opt", "dest", None)
+	with pytest.raises(CLIError):
+		cli.add_option("", "dest", None)
+	with pytest.raises(CLIError):
+		cli.add_option("opt", "", None)
+
+def test_remove_option():
+	cli = CLI("")
+	cli.add_option("option1", "dest", False)
+	cli.add_option("option2", "dest", True)
+	cli.add_option("option3", "dest", True)
+	cli.add_option("option4", "dest", False)
+	cli.add_option("option5", "dest", False)
+	cli.add_option("option6", "dest", False)
+
+	cli.remove_option("option3", "option6", "option1")
+	assert cli.options == {
+		"option2": ["dest", True],
+		"option4": ["dest", False],
+		"option5": ["dest", False],
+	}
+
